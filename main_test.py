d749542acd7eeb765aa41b20694c4549ce9d7a8d
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-03-19 14:43:41 +0530

    chore: add docstrings to tests
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- main_test.py
+++ main_test.py
@@ -4,12 +4,14 @@ from main import greeting
 
 
 def test_greeting():
+    """test_greeting checks that greeting returns the correct message"""
     msg = greeting()
     want = "hello there!"
     assert msg == want, f"the greeting is incorrect. Expected: {want} Actual: {msg}"
 
 
 def run_tests():
+    """run_tests runs all of the tests"""
     test_greeting()
 
 
